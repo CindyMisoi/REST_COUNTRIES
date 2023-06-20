6ee8b6451dc75a408b8c54214f449a57f01b0a18
CindyMisoi <crmisoi2k@gmail.com>
2023-06-20 16:14:00 +0300

    added a list of countries

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {useState, useEffect} from 'react';
+import '../styles/App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
+   const [error, setError] = useState(null);
+   const [isLoaded, setIsLoaded] = useState(false);
+   const [countries, setCountries] = useState([]);
+
+  //  empty deps array[] means that this useEffect will run once
+  useEffect(() => {
+    fetch(`https://restcountries.com/v2/all?fields=name,region,area`)
+    .then((response) => response.json())
+    .then((data) => {
+      setIsLoaded(true);
+      setCountries(data)
+    },
+    // error handling
+    (error) => {
+      setIsLoaded(true);
+      setError(error);
+    }
+    );
+  }, []);
+//  render a list of countries
+if(error){
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>Error: {error.message}</div>
   )
+}
+else if(!isLoaded){
+  return (
+    <div>
+      <p>Loading...</p>
+    </div>
+  )
+}
+else {
+  return (
+    <div className='wrapper'>
+      <ul className='card-grid'>
+        {countries.map((country) => (
+          <li>
+            <article className='card' key={country.id}>
+              <div className='card-content'>
+                <h2 className='card-name'>Country Name: {country.name}</h2>
+                <h2 className='card-region'>Region: {country.region}</h2>
+                <h2 className='card-area'>Area Size: {country.area}</h2>
+              </div>
+            </article>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
 }
 
 export default App
